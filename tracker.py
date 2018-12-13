7e303bb355f7bd06bb692d6407d2d7160a47d37f
longclaw <shivam.singhal212@gmail.com>
2018-12-13 22:44:56 +0530

    random url

--- tracker.py
+++ tracker.py
@@ -4,11 +4,11 @@ from flask import request, jsonify, send_file
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/image', methods=['GET'])
-def home():
+@app.route('/image/<string>', methods=['GET'])
+def home(string):
 	filename = 'ok.jpg'
 	print('iamge is opened.')
 	return send_file(filename, mimetype='image/gif')
 
 
-app.run()
+app.run(host='0.0.0.0', port=80)
